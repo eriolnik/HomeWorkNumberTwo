159c0999247d25a97a8758f7a54d55723a74fc50
eriolnik <lozarik16@yandex.ru>
2021-01-20 09:50:32 +0300

    Upgreat
    Upgreat

---
+++ cmake-build-debug/CMakeFiles/HomeWork2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HomeWork2.dir/main.cpp.o"
+  "HomeWork2"
+  "HomeWork2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HomeWork2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,1 +1,48 @@
+#include <iostream>
+#include <string>
+#include <fstream>
 
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+    if (argc != 3) {
+        cerr << "Error: Use three parameters\n";
+        return 5;
+    }
+
+    const string mode(argv[1]); // Режим работы
+    const string file_name(argv[2]); // Имя файла
+    if (mode == "encryption") { // Режим шифрование
+        char str;
+        cout << "Напишите текст длины 1 символ: \n";
+        cin >> str;
+        int password;
+        cout << "Ввведите пароль: " << endl << "Пароль: ";
+        cin >> password;
+        int8_t h = str;
+        srand(password);
+        h = h ^ rand();
+        h = (h << 2) | (h >> (8 - 2));
+        ofstream file;
+        //программа помещает значение в файл
+        file.open(file_name, ios::binary);
+        file << h;
+        file.close();
+    } else if (mode == "decryption") { // Режим расшифрования
+        //Программа получает зашифрованные данные из файла
+        int8_t n;
+        ifstream file;
+        file.open(file_name, ios::binary);
+        file >> n;
+        int password;
+        cout << "Введите пароль: " << endl << "Пароль: ";
+        cin >> password;
+        srand(password);
+        n = (n >> 2) | (n << (8 - 2));
+        n = n ^ rand();
+        char b = n;
+        cout << b;
+
+    }
+    return 0;
+}
